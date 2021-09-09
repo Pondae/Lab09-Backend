1b637960255382980c4d4281b023eced388356be
Domdae <domdae@Phonmongkons-MacBook-Pro.local>
2021-09-09 14:53:09 +0700

    NullPointerException

--- src/main/java/se331/lab/rest/config/InitApp.java
+++ src/main/java/se331/lab/rest/config/InitApp.java
@@ -5,42 +5,60 @@ import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 import se331.lab.rest.entity.Event;
+import se331.lab.rest.entity.Organizer;
 import se331.lab.rest.repository.EventRepository;
+import se331.lab.rest.repository.OrganizerRepository;
 
 @Component
 public class InitApp implements ApplicationListener<ApplicationReadyEvent> {
     @Autowired
     EventRepository eventRepository;
+    @Autowired
+    OrganizerRepository organizerRepository;
     @Override
     public void onApplicationEvent(ApplicationReadyEvent applicationReadyEvent) {
-        eventRepository.save(Event.builder()
-                .category("Academic")
-                .title("Midterm Exam")
-                .description("A time for taking the exam")
-                .location("CAMT Building")
-                .date("3rd Sept")
-                .time("3.00-4.00 pm.")
-                .petAllowed(false)
-                .build());
-        eventRepository.save(Event.builder()
-                .category("Academic")
-                .title("Commencement Day")
-                .description("A time for celebration")
-                .location("CMU Convention hall")
-                .date("21th Jan")
-                .time("8.00am-4.00 pm.")
-                .petAllowed(false)
-                .build());
-        eventRepository.save(Event.builder()
-                .category("Cultural")
-                .title("Loy Krathong")
-                .description("A time for Krathong")
-                .location("Ping River")
-                .date("21th Nov")
-                .time("8.00-10.00 pm.")
-                .petAllowed(false)
-                .build());
-        eventRepository.save(Event.builder()
+        Organizer org1, org2, org3;
+        org1 = organizerRepository.save(Organizer.builder()
+            .name("CAMT").build());
+        org2 = organizerRepository.save(Organizer.builder()
+                .name("CMU").build());
+        org3 = organizerRepository.save(Organizer.builder()
+                .name("ChiangMai").build());
+        Event tempEvent;
+        tempEvent = eventRepository.save(Event.builder()
+                    .category("Academic")
+                    .title("Midterm Exam")
+                    .description("A time for taking the exam")
+                    .location("CAMT Building")
+                    .date("3rd Sept")
+                    .time("3.00-4.00 pm.")
+                    .petAllowed(false)
+                    .build());
+        tempEvent.setOrganizer(org1);
+        org1.getOwnEvents().add(tempEvent);
+        tempEvent = eventRepository.save(Event.builder()
+                    .category("Academic")
+                    .title("Commencement Day")
+                    .description("A time for celebration")
+                    .location("CMU Convention hall")
+                    .date("21th Jan")
+                    .time("8.00am-4.00 pm.")
+                    .petAllowed(false)
+                    .build());
+        tempEvent.setOrganizer(org2);
+        org2.getOwnEvents().add(tempEvent);
+        tempEvent = eventRepository.save(Event.builder()
+                    .category("Cultural")
+                    .title("Loy Krathong")
+                    .description("A time for Krathong")
+                    .location("Ping River")
+                    .date("21th Nov")
+                    .time("8.00-10.00 pm.")
+                    .petAllowed(false)
+                    .build());
+        tempEvent.setOrganizer(org3);
+        org3.getOwnEvents().add(tempEvent);
+        tempEvent = eventRepository.save(Event.builder()
                 .category("Cultural")
                 .title("Songkran")
                 .description("Let's Play Water")
